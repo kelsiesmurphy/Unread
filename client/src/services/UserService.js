1d10be67c038c1214c4efdfd49ab23cfa08a55d9
Delfland <d_dallison@hotmail.com>
2023-01-24 15:40:54 +0000

    can create a user in db

---
+++ client/src/services/UserService.js
@@ -0,0 +1,37 @@
+const baseURL = 'http://localhost:9000/api/users';
+
+const UserService = {
+
+    // Adding a user to the database
+    addUser(user) {
+    return fetch(baseURL, {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(res => res.json());
+},
+
+//    get single user from d
+getSingleUser(id) {
+    return fetch(baseURL + "/" + id)
+    .then(res => res.json());
+},
+
+// Updating a user
+updateUser(user) {
+    return fetch(baseURL + user._id, {
+        method: 'PUT',
+        body: JSON.stringify(user),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(res => res.json());
+},
+
+}
+
+export default UserService;
